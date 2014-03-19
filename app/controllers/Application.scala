6a60d200b9657b2bb9e977a30524feb8f2f80e45
Fabio Tiriticco <fabio.europe@gmail.com>
2014-03-19 18:58:29 +0100

    proxying another web service

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,12 @@ package controllers
 import play.api.mvc._
 import play.api.libs.iteratee.{Iteratee, Concurrent, Enumerator}
 import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
+import scala.concurrent.{Await, Future}
 import play.api.Logger
 import play.api.libs.concurrent.Promise
 import java.io.File
+import play.api.libs.ws.WS
+import scala.concurrent.duration.DurationInt
 
 object Application extends Controller {
 
@@ -93,4 +95,39 @@ object Application extends Controller {
       (Iteratee.ignore[Array[Byte]], outEnumerator.andThen(Enumerator.eof))
     }
   }
+
+  // proxies another webservice
+  def httpWeatherProxy = Action.async {
+    request => {
+      val url = "http://api.openweathermap.org/data/2.5/weather?q=Amsterdam,nl"
+      WS.url(url).get().map(r => Ok(r.body))
+    }
+  }
+
+  // proxies another webservice, websocket style
+  def wsWeatherProxy = WebSocket.async[String] {
+    request => Future {
+      val url = "http://api.openweathermap.org/data/2.5/weather?q=Amsterdam,nl"
+      var switch = false
+      val myEnumerator: Enumerator[String] = Enumerator.generateM[String](WS.url(url).get().map(r => {
+        switch = !switch
+        if (switch) Option(r.body)
+        else None
+      }))
+      (Iteratee.ignore[String], myEnumerator)
+    }
+  }
+
+  // watch out! haven't got this working yet!
+  def wsWeatherExp = WebSocket.async[String] {
+    request => Future {
+      val interval = 3 seconds
+      val url = "http://api.openweathermap.org/data/2.5/weather?q=Amsterdam,nl"
+      val w: Future[String] = WS.url(url).get().map(r => r.body)
+      val t = Promise.timeout(w, 5000)
+      val myEnumerator = Enumerator.repeatM[String](t)
+      (Iteratee.ignore[String], myEnumerator)
+    }
+  }
+
 }
